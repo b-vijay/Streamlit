0a1923ae500fe6543f5efba4b4c90c3abb584d14
b-vijay <brindavijayakumar@gmail.com>
2024-06-25 12:03:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,9 +76,6 @@ st.metric(label="Overall Profit Margin (%)", value=f"{overall_profit_margin:.2f}
 
 
 
-
-
-st.write("## Your additions")
 st.write("### (1) add a drop down for Category (https://docs.streamlit.io/library/api-reference/widgets/st.selectbox)")
 st.write("### (2) add a multi-select for Sub_Category *in the selected Category (1)* (https://docs.streamlit.io/library/api-reference/widgets/st.multiselect)")
 st.write("### (3) show a line chart of sales for the selected items in (2)")
