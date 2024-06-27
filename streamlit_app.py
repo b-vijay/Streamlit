515bcf9cafa6d5d31bc560b2577f3a25f08b457a
b-vijay <brindavijayakumar@gmail.com>
2024-06-26 20:57:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,7 +70,6 @@ st.metric(label="Overall Profit Margin (%)", value=f"{overall_profit_margin:.2f}
 
 
 
-st.write("### (4) show three metrics (https://docs.streamlit.io/library/api-reference/data/st.metric) for the selected items in (2): total sales, total profit, and overall profit margin (%)")
-st.write("### (5) use the delta option in the overall profit margin metric to show the difference between the overall average profit margin (all products across all categories)")
+
 
 
